78659b2bececb803a37fb2b6c203ac0763da072a
jhondev <jma9974@gmail.com>
2023-02-03 14:00:34 -0500

    ip rate limiting based

--- apps/greenlight/cmd/api/main.go
+++ apps/greenlight/cmd/api/main.go
@@ -26,6 +26,14 @@ type config struct {
 		maxIdleConns int
 		maxIdleTime  string
 	}
+	// limiter struct containing fields for the requests-per-second and burst
+	// values, and a boolean field which we can use to enable/disable rate limiting
+	// altogether.
+	limiter struct {
+		rps     float64 // requests per second
+		burst   int
+		enabled bool
+	}
 }
 
 type application struct {
@@ -45,6 +53,11 @@ func main() {
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
+	// Notice that we use true as the default for the 'enabled' setting?
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
 	flag.Parse()
 
